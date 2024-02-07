51c91b5782fc3a1fad26ba0757a9fb7265612bb0
takeru.hashimoto <hstake25@gmail.com>
2024-02-07 22:34:10 +0900

    go cron

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/robfig/cron/v3"
+)
+
+func main() {
+	c := cron.New()
+	c.AddFunc("@every 1s", func() { log.Println("every 1s") })
+	c.AddFunc("@every 5s", SampleFunction)
+	c.Start()
+	time.Sleep(2 * time.Minute)
+}
+
+func SampleFunction() {
+	fmt.Println("Sample Function")
+}
